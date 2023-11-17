f892caa90802fee1f0191b706de73b862c1017fe
joanmrt_ <jmartinezs@cifpfbmoll.eu>
2023-11-17 20:43:43 +0100

    Update button

--- app/src/main/java/com/example/task13/MainActivity.java
+++ app/src/main/java/com/example/task13/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.task13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
